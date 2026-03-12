e2f93404a2e67ec6c2d0051d58880b7aa06afc92
Danielggz <dggz1993@gmail.com>
2026-03-12 10:36:46 +0000

    Added functions file

---
+++ functions.py
@@ -0,0 +1,39 @@
+#get cpu temperature
+def get_cpu_temp():
+    with open("/sys/class/thermal/thermal_zone0/temp", "r") as f:
+        return int(f.read()) / 1000.0
+
+#Sunlight estimates for Dublin
+SUN_HOURS = {
+    "N": 1,
+    "NE": 2,
+    "E": 3,
+    "SE": 5,
+    "S": 7,
+    "SW": 5,
+    "W": 3,
+    "NW": 1
+}
+
+#get orientation based on yaw data
+def yaw_to_direction(yaw):
+    dirs = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"]
+    idx = int((yaw + 22.5) // 45) % 8
+    return dirs[idx]
+
+#Get estimate based on orientation
+def estimate_sunlight_hours(yaw):
+    direction = yaw_to_direction(yaw)
+    return SUN_HOURS[direction]
+
+#csv writer
+def log_to_csv(csv_path, temperature, humidity, pressure, sunlight_hours, timestamp):
+    file_exists = os.path.exists(csv_path)
+
+    with open(csv_path, "a", newline="") as f:
+        writer = csv.writer(f)
+
+        if not file_exists:
+            writer.writerow(["timestamp", "temperature", "humidity", "sunlight_hours", "pressure"])
+
+        writer.writerow([timestamp, temperature, humidity, sunlight_hours, pressure])
--- sensePlantConditions.py
+++ sensePlantConditions.py
@@ -4,53 +4,63 @@ import json
 import time
 from datetime import datetime, timezone
 import random
-
+#CSV manipulation
+import csv
+import os
+#Sun conditions script
+from functions import estimate_sunlight_hours, get_cpu_temp
 #Get senseHat
 sense = SenseHat()
-
 #Time metrics
 TIME_INTERVAL = 5
 AVG_INTERVAL = 30
-
 #PowerBI push url to connect with report
-#PUSH_URL = "Yhttps://api.powerbi.com/beta/6edb49c1-bf72-4eea-8b3f-a7fd0a25b68c/datasets/c8bb2909-769b-423d-8512-777289f7aed0/rows?experience=power-bi&key=2dMXreV%2F4Nlplb2qAcjgl3tAmxzDL4asoxwYu8W%2FwSgXi5%2BuxQILz14%2FMYciF20KrVM6z0J5ABd1o5Vx26Ym1A%3D%3D"
-
+PUSH_URL = ""
 #array to store temperatures
 tempArray = []
-
+#timer
 start_time = time.time()
+#csv storage
+csv_file = "./data/conditionsData.csv"
+
+os.makedirs("./data", exist_ok=True)
+
+#Get orientation of the pi
+orientation = sense.get_orientation()
+yaw = orientation["yaw"]
+#Call method to estimate sunlight time for Dublin
+sun_hours = estimate_sunlight_hours(yaw)
 
 while True:
-    #get cpu temperature
-    def get_cpu_temp():
-        with open("/sys/class/thermal/thermal_zone0/temp", "r") as f:
-            return int(f.read()) / 1000.0
-    
     humidity = round(sense.get_humidity(), 2)
     pressure = round(sense.get_pressure(), 2)
     #To get a correct reading, deduct pi temperature from sensehat reading  
     temperature = round(sense.get_temperature(), 2)
+    timestamp = datetime.now(timezone.utc).isoformat()
     cpu_temp = get_cpu_temp()
     
-    finalTemp = temperature - ((cpu_temp - temperature) / 1.5)
+    finalTemp = round(temperature - ((cpu_temp - temperature) / 1.5),2)
     tempArray.append(finalTemp)
 
     data = [{
         "temperature": finalTemp,
         "humidity": humidity,
         "pressure": pressure,
-        "timestamp": datetime.now(timezone.utc).isoformat()
+        "sunlight_hours": sun_hours,
+        "timestamp": timestamp
     }]
+
+    log_to_csv(csv_file, finalTemp, humidity, pressure, sun_hours, timestamp)
 
     if time.time() - start_time >= AVG_INTERVAL:
         avg_time = sum(tempArray) / len(tempArray)        
-        print("Avg time ", AVG_INTERVAL, " seconds: ", avg_time)
+        print("Avg temperature in ", AVG_INTERVAL, " seconds: ", avg_time)
 
-        #Restart to new 30 seconds interval
+       #Restart to new 30 seconds interval
         start_time = time.time()
         tempArray.clear()
 
-    # response = requests.post(PUSH_URL, json=data)
+    #response = requests.post(PUSH_URL, json=data)
     #print("Status:", response.status_code, "Sent:", data)
     print(data)
     time.sleep(5)
