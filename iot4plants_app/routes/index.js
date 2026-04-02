18531c1d5d6cd875b47205bb944b9fb896b95c69
Danielggz <dggz1993@gmail.com>
2026-04-02 10:01:42 +0100

    Add powerbi plots to frontend. Rearranged data files

--- iot4plants_app/routes/index.js
+++ iot4plants_app/routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+// PowerBI report
+var reportIframe = "https://app.powerbi.com/reportEmbed?reportId=df7dbc26-049c-4613-a755-5ca32243a9d6&autoAuth=true&ctid=6edb49c1-bf72-4eea-8b3f-a7fd0a25b68c"
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Plant Conditions report', iframeUrl: reportIframe });
 });
 
 module.exports = router;
